12ceb83bcff56d61b4278e6b3c5d99cbfde72827
User <User@LAPTOP-CH4L909N>
2020-09-08 17:56:08 +0200

    Initial commit

---
+++ CarRentApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using CarRentApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
